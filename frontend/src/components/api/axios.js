e7f95cd357049db86cb63871410ad3d82aba3c91
SurajKumar798 <128129426+SurajKumar798@users.noreply.github.com>
2026-03-06 23:16:13 +0530

    Update axios.js

--- frontend/src/components/api/axios.js
+++ frontend/src/components/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:4000',
+    baseURL: 'https://event-management-platform-backend-6j3t.onrender.com',
 })
 
 export default api;
